9bed31797f93965aba1288ed3e33aa718ae5fc09
Wolfertt <arend.hamoen@gmail.com>
2023-06-06 12:41:18 +0200

    add basis

--- snackSite/Pages/Index.cshtml.cs
+++ snackSite/Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using snackSite.Models;
+using snackSite.Repositories;
 
 namespace snackSite.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<Product> Producten { get; set; } = null!;
+        
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -12,9 +16,14 @@ namespace snackSite.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet(string? searchTerm = null)
         {
-
+            Producten = new ProductenRepository().GetProduct();
+            
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                Producten = Producten.Where(Producten => Producten.ProductNaam.Contains(searchTerm));
+            }
         }
     }
 }
