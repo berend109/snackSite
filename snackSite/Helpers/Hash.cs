82d714c34ea4f42fad82921fc877106ae04bdd96
Berend Folkers <16764030+berend109@users.noreply.github.com>
2023-06-09 05:44:06 +0200

    .

--- snackSite/Helpers/Hash.cs
+++ snackSite/Helpers/Hash.cs
@@ -3,14 +3,14 @@ using System.Text;
 
 namespace snackSite.Helper;
 
-public static class Hasj
+public static class Hash
 {
     /// <summary>
     /// using sha256 for hashing the password.
     /// </summary>
     /// <param name="password"></param>
     /// <returns>hashed password</returns>
-    public static string HashPassword(string password)
+    public static string HashPassword(string? password)
     {
         using var sha256 = SHA256.Create();
         var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
